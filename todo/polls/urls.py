2b3d55cac19e3009c42394d7fa57750b79acad5b
belldivine070 <belldivine070@gmail.com>
2025-01-30 08:01:07 +0100

    Delete todo directory

--- todo/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import  path
-from .import views
-
-
-urlpatterns = [
-    path("", views.Index.as_view(), name="Index"),
-    path("Login/", views.Login.as_view(), name="Login"),
-    path("Login/Home/<int:pk>/", views.Home.as_view(), name="Home")
-]
--- todo/polls/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Signup, Home
-from .forms import signUpForm, loginForm
-from django.urls import reverse_lazy
-
- 
